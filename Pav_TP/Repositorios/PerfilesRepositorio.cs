cf2d61ed2c11f9cc6d518814e6ec62e45ca2f401
Nacho Suarez <ignaciosuarez321@gmail.com>
2022-09-15 00:30:26 -0300

    PerfilesRepo/PerfilesServ

---
+++ Pav_TP/Repositorios/PerfilesRepositorio.cs
@@ -0,0 +1,29 @@
+﻿using Pav_TP.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pav_TP.Repositorios
+{
+    public class PerfilesRepositorio
+    {
+        public List<Perfil> GetPerfiles()
+        {
+            var sql = $"Select * from perfiles";
+            var tabla = DBHelper.GetDBHelper().ConsultaSQL(sql);
+            var perfiles = new List<Perfil>();
+            foreach (DataRow fila in tabla.Rows)
+            {
+                var perfil = new Perfil();
+                perfil.Id = Convert.ToInt64(fila["id_perfil"].ToString());
+                perfil.Nombre = fila["nombre"].ToString();
+                perfiles.Add(perfil);
+            }
+
+            return perfiles;
+        }
+    }
+}
