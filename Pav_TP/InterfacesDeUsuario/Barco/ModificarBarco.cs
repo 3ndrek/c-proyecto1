331beb3552e7ea360c4366ab38a94f13b585139e
Tomas Tiseira <tomy.tiseira.22hh@gmail.com>
2022-09-16 12:04:06 -0300

    Agregando barco e itinerario

--- Pav_TP/InterfacesDeUsuario/Barco/ModificarBarco.cs
+++ Pav_TP/InterfacesDeUsuario/Barco/ModificarBarco.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Pav_TP
+namespace Pav_TP.InterfacesDeUsuario.Barco
 {
-    public partial class Form1 : Form
+    public partial class ModificarBarco : Form
     {
-        public Form1()
+        public ModificarBarco()
         {
             InitializeComponent();
         }
